07cb8e67b0ba0f2fd651a22a3f9e9529b7b7d2e2
Timothy Teräväinen <teravainen@gmail.com>
2020-04-18 02:31:40 -0400

    tensorflow 2.1.0 support

--- src/generate_unconditional_samples.py
+++ src/generate_unconditional_samples.py
@@ -9,15 +9,15 @@ import tensorflow as tf
 import model, sample, encoder
 
 def sample_model(
-    model_name='124M',
-    seed=None,
-    nsamples=0,
-    batch_size=1,
-    length=None,
-    temperature=1,
-    top_k=0,
-    top_p=1,
-    models_dir='models',
+        model_name='1558M',
+        seed=None,
+        nsamples=0,
+        batch_size=1,
+        length=None,
+        temperature=1,
+        top_k=40,
+        top_p=1,
+        models_dir='models',
 ):
     """
     Run the sample_model
@@ -51,9 +51,9 @@ def sample_model(
     elif length > hparams.n_ctx:
         raise ValueError("Can't get samples longer than window size: %s" % hparams.n_ctx)
 
-    with tf.Session(graph=tf.Graph()) as sess:
+    with tf.compat.v1.Session(graph=tf.Graph()) as sess:
         np.random.seed(seed)
-        tf.set_random_seed(seed)
+        tf.compat.v1.set_random_seed(seed)
 
         output = sample.sample_sequence(
             hparams=hparams, length=length,
@@ -62,8 +62,8 @@ def sample_model(
             temperature=temperature, top_k=top_k, top_p=top_p
         )[:, 1:]
 
-        saver = tf.train.Saver()
-        ckpt = tf.train.latest_checkpoint(os.path.join(models_dir, model_name))
+        saver = tf.compat.v1.train.Saver()
+        ckpt = tf.compat.v1.train.latest_checkpoint(os.path.join(models_dir, model_name))
         saver.restore(sess, ckpt)
 
         generated = 0
--- src/interactive_conditional_samples.py
+++ src/interactive_conditional_samples.py
@@ -9,7 +9,7 @@ import tensorflow as tf
 import model, sample, encoder
 
 def interact_model(
-    model_name='124M',
+    model_name='1558M',
     seed=None,
     nsamples=1,
     batch_size=1,
@@ -54,10 +54,10 @@ def interact_model(
     elif length > hparams.n_ctx:
         raise ValueError("Can't get samples longer than window size: %s" % hparams.n_ctx)
 
-    with tf.Session(graph=tf.Graph()) as sess:
-        context = tf.placeholder(tf.int32, [batch_size, None])
+    with tf.compat.v1.Session(graph=tf.Graph()) as sess:
+        context = tf.compat.v1.placeholder(tf.int32, [batch_size, None])
         np.random.seed(seed)
-        tf.set_random_seed(seed)
+        tf.compat.v1.set_random_seed(seed)
         output = sample.sample_sequence(
             hparams=hparams, length=length,
             context=context,
@@ -65,8 +65,8 @@ def interact_model(
             temperature=temperature, top_k=top_k, top_p=top_p
         )
 
-        saver = tf.train.Saver()
-        ckpt = tf.train.latest_checkpoint(os.path.join(models_dir, model_name))
+        saver = tf.compat.v1.train.Saver()
+        ckpt = tf.compat.v1.train.latest_checkpoint(os.path.join(models_dir, model_name))
         saver.restore(sess, ckpt)
 
         while True:
